6e435a066124fa02972ca3a37b2e58e576422e4e
reha96 <reha.tuncer@gmail.com>
2026-07-10 17:22:57 +0200

    Update 3-clean_total_charges.py

--- data_analysis/data_preparation_visualization/3-clean_total_charges.py
+++ data_analysis/data_preparation_visualization/3-clean_total_charges.py
@@ -11,15 +11,14 @@ def clean_total_charges(df, method='drop'):
         df (_type_): _description_
         method (str, optional): _description_. Defaults to 'drop'.
     """
-    convert_columns = __import__('2-convert_columns').convert_columns
-    convert_columns(df)
+    out = df.copy()
     if method == 'drop':
-        df = df.dropna(subset=['TotalCharges'])  # only for said col
+        out = df.dropna(subset=['TotalCharges'])  # only for said col
     elif method == 'median':
-        df['TotalCharges'] = df['TotalCharges'].fillna(
+        out['TotalCharges'] = df['TotalCharges'].fillna(
             df['TotalCharges'].median())
     elif method == 'impute':
-        df['TotalCharges'] = df['TotalCharges'].fillna(
+        out['TotalCharges'] = df['TotalCharges'].fillna(
             df['MonthlyCharges'] * df['tenure'])
 
-    return df
+    return out
