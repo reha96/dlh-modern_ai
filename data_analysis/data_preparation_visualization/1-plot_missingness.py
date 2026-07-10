fa098cf37e81e4f130cd75697f61261fb3c17340
reha96 <reha.tuncer@gmail.com>
2026-07-10 14:35:20 +0200

    Update 1-plot_missingness.py

--- data_analysis/data_preparation_visualization/1-plot_missingness.py
+++ data_analysis/data_preparation_visualization/1-plot_missingness.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 """
+Write a function def plot_missingness(df):
+that visualizes missing values in a DataFrame
 """
 import matplotlib.pyplot as plt
 import numpy as np
@@ -7,10 +9,12 @@ import numpy as np
 
 def plot_missingness(df):
     """
+    visualizes missing values in a DataFrame
     """
     plt.figure(figsize=(12, 8))
 
-    plt.scatter(df.isna().sum(), df.columns, marker="|")
+    row_idx, col_idx = np.where(df.isnull().values)
+    plt.scatter(row_idx, col_idx, marker="|")
 
     plt.tight_layout()
     plt.show()
