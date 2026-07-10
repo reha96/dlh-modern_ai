e58256bbb3b838c540392786034fb1bb65e22079
reha96 <reha.tuncer@gmail.com>
2026-07-10 15:53:53 +0200

    Update 2-convert_columns.py

--- data_analysis/data_preparation_visualization/2-convert_columns.py
+++ data_analysis/data_preparation_visualization/2-convert_columns.py
@@ -12,11 +12,13 @@ def convert_columns(df):
     Args:
         df (_type_): _description_
     """
-    df['TotalCharges'] = pd.to_numeric(df['TotalCharges'], errors='coerce')
+    df['TotalCharges'] = pd.to_numeric(
+        # coerce converts non-numeric to NaN
+        df['TotalCharges'], errors='coerce')
     df['SeniorCitizen'] = df['SeniorCitizen'].replace(
         {
             0: 'No',
             1: 'Yes'
         }
-    )
+    )  # replace leaves un-mapped values as is
     return df
