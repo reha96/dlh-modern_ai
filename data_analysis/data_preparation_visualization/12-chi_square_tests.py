5a2f684e4df9f2f190ddb5b6135a3d5b7bd324b9
reha96 <reha.tuncer@gmail.com>
2026-07-15 12:31:17 +0200

    Create 12-chi_square_tests.py

---
+++ data_analysis/data_preparation_visualization/12-chi_square_tests.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+"""Write a function that performs chi-square tests
+for categorical features, using SciPy:
+    """
+
+import pandas as pd
+from scipy import stats
+
+
+def chi_square_tests(df):
+    """df: pandas DataFrame with Churn and categorical columns
+
+Computes the Chi-square p-value to test the independence
+between each categorical feature and the target variable Churn,
+excluding Churn itself from the features tested.
+
+Returns a dictionary: {feature_name: p_value}
+
+    Args:
+        df (_type_): _description_
+    """
+    out = {}
+    
+    for col in df:
+        table = pd.crosstab(df[col], df['Churn'])
+        _, p, _, _ = stats.chi2_contingency(table)
+        out[col] = p
+        
+    return out
+
+
+df = pd.read_csv('data_analysis/data_preparation_visualization/precleaned-Telco-Customer-Churn.csv')
+print(chi_square_tests(df))
