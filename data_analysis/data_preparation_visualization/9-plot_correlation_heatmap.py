cfd3c5c1fa702d799fe5146204e0f289e9973683
reha96 <reha.tuncer@gmail.com>
2026-07-15 08:23:50 +0200

    Create 9-plot_correlation_heatmap.py

---
+++ data_analysis/data_preparation_visualization/9-plot_correlation_heatmap.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Write a function that visualizes correlations between
+continuous numeric features using seaborn
+"""
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+
+def plot_correlation_heatmap(df):
+    """
+    df: pandas DataFrame
+Computes pairwise correlations
+Generates an annotated heatmap with coolwarm colormap
+Set vmin = -1 and vmax = 1 so that the heatmap color mapping
+reflects the full correlation range
+Displays the plot
+Returns: None
+    """
+    plt.figure(figsize=(6, 5))
+    df = df.select_dtypes(include='number')
+    df = df.corr()
+    sns.heatmap(df, vmin=-1, vmax=1, cmap='coolwarm')
