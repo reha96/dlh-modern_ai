4e4f8bd5a743bbc87fee639ec87d5804f4e2e269
reha96 <reha.tuncer@gmail.com>
2026-07-18 09:43:47 +0200

    Update 14-create_features.py

--- data_analysis/data_preparation_visualization/14-create_features.py
+++ data_analysis/data_preparation_visualization/14-create_features.py
@@ -30,8 +30,8 @@ Returns the modified DataFrame
         df (_type_): _description_
     """
     services = ['OnlineSecurity', 'OnlineBackup', 'DeviceProtection',
-                'TechSupport', 'StreamingTV', \
-                    'StreamingMovies', 'MultipleLines']
+                'TechSupport', 'StreamingTV',
+                'StreamingMovies', 'MultipleLines']
 
     df = pd.DataFrame(df)  # to access function helpers, DF
     # Count 'Yes' across service columns
