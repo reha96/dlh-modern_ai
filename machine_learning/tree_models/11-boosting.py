d5b59064f43e1c221615dc5d80b52690ef2b2326
reha96 <reha.tuncer@gmail.com>
2026-07-19 21:25:41 +0200

    Create 11-boosting.py

---
+++ machine_learning/tree_models/11-boosting.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+"""Write a function that initializes and returns an untrained boosting
+classifier based on the specified algorithm name.
+    """
+
+from sklearn import ensemble
+import xgboost as xgb
+import lightgbm as lgb
+
+
+def compare_boosting_classifiers(name, n_estimators, random_state):
+    """Initialize the selected model with the specified n_estimators
+    and random_state.
+    For LightGBM, verbose is set to -1 to suppress training logs.
+    Raise a ValueError with the message “Unknown model name '{name}'”
+    if the provided model name is invalid.
+
+Arguments:
+
+    name (str): Name of the boosting algorithm. Must be one of:
+    'adaboost' — returns an AdaBoostClassifier
+    'gradientboosting' — returns a GradientBoostingClassifier
+    'xgboost' — returns an XGBClassifier
+    'lightgbm' — returns an LGBMClassifier
+    n_estimators (int): Number of boosting iterations (trees).
+    random_state (int): Random seed for reproducibility.
+
+Returns:
+
+    An untrained instance of the selected boosting classifier.
+    """
+    if name == 'adaboost':
+        return ensemble.AdaBoostClassifier(n_estimators=n_estimators, random_state=random_state)
+    elif name == 'gradientboosting':
+        return ensemble.GradientBoostingClassifier(
+            n_estimators=n_estimators, random_state=random_state)
+    elif name == 'xgboost':
+        return xgb.XGBClassifier(n_estimators=n_estimators,
+                                 random_state=random_state)
+    elif name == 'lightgbm':
+        return lgb.LGBMClassifier(n_estimators=n_estimators,
+                                  random_state=random_state, verbose=-1)
+    else:
+        raise ValueError(f"Unknown model name '{name}'")
