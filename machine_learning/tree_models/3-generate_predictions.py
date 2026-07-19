6ec3513ab352b7c17425186757c852ef815ec3bd
reha96 <reha.tuncer@gmail.com>
2026-07-19 20:06:07 +0200

    cr gitignore and 3-

---
+++ machine_learning/tree_models/3-generate_predictions.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Write a function to generate predictions
+from a trained tree-based classifier using Scikit-learn.
+"""
+
+from sklearn import tree
+
+
+def generate_predictions(clf, X):
+    """Arguments:
+
+clf: A trained Scikit-learn classifier instance
+X: Feature matrix (NumPy array or pandas DataFrame)
+
+Returns:
+
+A NumPy array containing the predicted class labels
+for the input samples.
+    """
+    return clf.predict(X)
