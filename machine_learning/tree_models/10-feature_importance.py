f29020ea5e4d4b0025bef304df9197d542e05952
reha96 <reha.tuncer@gmail.com>
2026-07-19 21:19:02 +0200

    Create 10-feature_importance.py

---
+++ machine_learning/tree_models/10-feature_importance.py
@@ -0,0 +1,22 @@
+#!/bin/usr/env python3
+"""Write a function that computes and returns the feature importances
+from a trained random forest model.
+    """
+import numpy as np
+
+
+def feature_importance(rf):
+    """Arguments:
+
+    rf: A trained Scikit-learn RandomForestClassifier instance.
+
+Returns:
+
+    importances: A NumPy array of feature importance scores.
+    indices: A NumPy array of feature indices sorted from least
+    to most important (ascending order).
+
+    """
+    importances = rf.feature_importances_
+    indices = np.argsort(importances)
+    return importances, indices
