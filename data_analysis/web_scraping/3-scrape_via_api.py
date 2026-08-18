0191e8c2bc4468f28a1a568654cabe0a10514c5e
reha96 <reha.tuncer@gmail.com>
2026-08-19 00:15:23 +0200

    Update 3-scrape_via_api.py

--- data_analysis/web_scraping/3-scrape_via_api.py
+++ data_analysis/web_scraping/3-scrape_via_api.py
@@ -22,7 +22,7 @@ Returns: a list of quote dictionaries
     # create page counter
     count = 1
     # create API endpoint
-    api_url = base_url + f"/api/quotes?page={count}"
+    api_url = base_url + f"api/quotes?page={count}"
 
     out = []
 
@@ -48,4 +48,4 @@ Returns: a list of quote dictionaries
 
         # build the next API endpoint
         count += 1
-        api_url = base_url + f'/api/quotes?page={count}'
+        api_url = base_url + f'api/quotes?page={count}'
