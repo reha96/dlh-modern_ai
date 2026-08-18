42acddda8adbdc1f66c7c9f04a5e28b491d7b28c
reha96 <reha.tuncer@gmail.com>
2026-08-19 00:11:40 +0200

    Update 3-scrape_via_api.py

--- data_analysis/web_scraping/3-scrape_via_api.py
+++ data_analysis/web_scraping/3-scrape_via_api.py
@@ -19,4 +19,33 @@ Extract text, the author's name, and tags from each quote.
 
 Returns: a list of quote dictionaries
     """
-    pass
+    # create page counter
+    count = 1
+    # create API endpoint
+    api_url = base_url + f"/api/quotes?page={count}"
+
+    out = []
+
+    while api_url:
+
+        # fetch the JSON text
+        response_text = fetch_html(api_url)
+
+        # convert JSON text into Python data
+        payload = json.loads(response_text)
+
+        # add each quote from this page
+        for quote in payload['quotes']:
+            out.append({
+                'text': quote['text'],
+                'author': quote['author']['name'],
+                'tags': quote['tags']
+            })
+
+        # stop when this is the last page
+        if not payload['has_next']:
+            break
+
+        # build the next API endpoint
+        count += 1
+        api_url = base_url + f'/api/quotes?page={count}'
