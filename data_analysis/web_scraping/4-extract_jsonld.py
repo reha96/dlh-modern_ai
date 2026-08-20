4fbe2856a6b882bc576dc45c7c048b8c21b99549
reha96 <reha.tuncer@gmail.com>
2026-08-20 11:32:24 +0200

    Update 4-extract_jsonld.py

--- data_analysis/web_scraping/4-extract_jsonld.py
+++ data_analysis/web_scraping/4-extract_jsonld.py
@@ -21,4 +21,27 @@ Extract text, the author's name, and tags from Quote objects.
 
 Returns: a list of quote dictionaries
     """
-    pass
+    # fetch the page and turn the html into a searchable soup object
+    soup = BeautifulSoup(fetch_html(url), 'html.parser')
+
+    # find each application/ld+json script
+    # use find_all because page has many
+    scripts = soup.find_all('script', type='application/ld+json')
+
+    out = []
+    for script in scripts:
+        # parse script
+        # json.loads needs a string so use .get_text()
+        payload = json.loads(script.get_text())
+
+        # extract
+        if payload.get("@type") == "Quote":
+            out.append({
+                "text": payload.get("text"),
+                # {} is fallback for empty author (return empty dict)
+                "author": payload.get("author", {}).get("name"),
+                # [] is fallback for empty tags (return empty list)
+                "tags": payload.get("keywords", [])
+            })
+
+    return out
