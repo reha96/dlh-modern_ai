46c52c1bf0d209c7cac77632e7b3d98e7be5aeee
reha96 <reha.tuncer@gmail.com>
2026-08-20 11:55:42 +0200

    Update 5-login_and_scrape.py

--- data_analysis/web_scraping/5-login_and_scrape.py
+++ data_analysis/web_scraping/5-login_and_scrape.py
@@ -30,8 +30,8 @@ Returns: a list of quote dictionaries
     # convert r to text
     soup = BeautifulSoup(r.text, 'html.parser')
 
-    # find unique token
-    token = soup.find('input', {'name': 'csrf_token'}).get('value')
+    # find unique token with fallback
+    token = soup.find('input', {'name': 'csrf_token'}).get('value', None)
 
     # POST back username and pwd
     post = s.post(login_url, data={'username': user,
